75b975c343d4b3ecd71c829fd7c6401db2e30039
karugo-murage <murage.gidion@student.moringaschool.com>
2024-09-24 14:24:02 +0300

    Add intruction.js content

---
+++ src/components/instruction.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Instruction = () => {
+    return ( 
+        <>
+            <div className="instructions">  
+                <p>Proper position sizing is key to managing risk and to avoid blowing out your account on a single trade.</p> 
+                <p>To use the position size calculator, enter the ammount you are risking in $.</p>
+                <p>Our position sizing calculator will suggest position sizes based on the information you provide.</p>
+                <p> The position size calculator assumes the you are trading XXXUSD pairs e.g GBPUSD</p>
+            </div>
+        </>
+     );
+}
+ 
+export default Instruction;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Instruction from './components/instruction';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Instruction/>
   </React.StrictMode>
 );
 
