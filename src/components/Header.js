9124c396176a3adf3c690cc86d30ac692ad4e413
karugo-murage <murage.gidion@student.moringaschool.com>
2024-09-24 14:28:40 +0300

    add header content

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+    return (  
+        <>
+            <div>POSITION SIZE CALCULATOR</div>
+            <div className="subheader">with commission intergration</div>
+        </>
+    );
+}
+ 
+export default Header;
