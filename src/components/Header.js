e8f57538f7f3d743991f446e60932b106be16cf8
karugo-murage <murage.gidion@student.moringaschool.com>
2024-10-06 02:59:34 +0300

    Style header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = ({ toggleTheme, isDarkMode }) => {
         <>
         <div className="heading">
              <div className="header">POSITION SIZE CALCULATOR</div>
-            <div className="subheader">with commission intergration</div>
+            <div className="subheader"> ~with commission intergration~</div>
             <button className="theme-toggle" onClick={toggleTheme}>
                     {isDarkMode ? "Switch to Light Mode" : "Switch to Dark Mode"}
                 </button>
