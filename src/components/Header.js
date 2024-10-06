78ec470761b80f26cbe2e9360e21dc1a5cbaa4c5
karugo-murage <murage.gidion@student.moringaschool.com>
2024-10-06 17:32:09 +0300

    add final changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,22 @@
 import React from "react";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
 
 const Header = ({ toggleTheme, isDarkMode }) => {
     return (  
         <>
         <div className="heading">
-             <div className="header">POSITION SIZE CALCULATOR</div>
-            <div className="subheader"> ~with commission intergration~</div>
-            <button className="theme-toggle" onClick={toggleTheme}>
-                    {isDarkMode ? "Switch to Light Mode" : "Switch to Dark Mode"}
-                </button>
+            <div className="header">POSITION SIZE CALCULATOR</div>
+            <div className="subheaderbox">
+                <div className="subheader"> ~with commission intergration~</div>
+                <div className="theme-icon" onClick={toggleTheme}>
+                    {isDarkMode ? (
+                        <i className="fas fa-sun"></i> // Sun icon for light mode
+                    ) : (
+                        <i className="fas fa-moon"></i> // Moon icon for dark mode
+                    )}
+                </div>
+            </div>  
         </div>
          
         </>
